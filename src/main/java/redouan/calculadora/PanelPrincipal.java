c615198b59d21eeef3252fc3cb2e9d53cd7640d6
reduan96 <redu1996@gmail.com>
2021-03-27 09:43:57 +0100

    Funcionalidad muy básica de los botones

--- src/main/java/redouan/calculadora/PanelPrincipal.java
+++ src/main/java/redouan/calculadora/PanelPrincipal.java
@@ -7,6 +7,8 @@ package redouan.calculadora;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -14,7 +16,7 @@ import javax.swing.JTextArea;
  *
  * @author redu2
  */
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements MouseListener {
 
     // Atributos de la clase (privados)
     private PanelBotones botonera;
@@ -41,6 +43,105 @@ public class PanelPrincipal extends JPanel {
         // Colocamos la botonera y el área texto
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
+
+        //Añadimos el daemon de escuha para la botonera
+        for (int i = 0; i < botonera.grupoBotones.length; i++) {
+
+            botonera.grupoBotones[i].addMouseListener(this);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        if (e.getSource() == botonera.grupoBotones[0]) {
+
+            areaTexto.setText(areaTexto.getText() + "0");
+
+        } else if (e.getSource() == botonera.grupoBotones[1]) {
+
+            areaTexto.setText(areaTexto.getText() + "1");
+
+        } else if (e.getSource() == botonera.grupoBotones[2]) {
+
+            areaTexto.setText(areaTexto.getText() + "2");
+
+        } else if (e.getSource() == botonera.grupoBotones[3]) {
+
+            areaTexto.setText(areaTexto.getText() + "3");
+
+        } else if (e.getSource() == botonera.grupoBotones[4]) {
+
+            areaTexto.setText(areaTexto.getText() + "4");
+
+        } else if (e.getSource() == botonera.grupoBotones[5]) {
+
+            areaTexto.setText(areaTexto.getText() + "5");
+
+        } else if (e.getSource() == botonera.grupoBotones[6]) {
+
+            areaTexto.setText(areaTexto.getText() + "6");
+
+        } else if (e.getSource() == botonera.grupoBotones[7]) {
+
+            areaTexto.setText(areaTexto.getText() + "7");
+
+        } else if (e.getSource() == botonera.grupoBotones[8]) {
+
+            areaTexto.setText(areaTexto.getText() + "8");
+
+        } else if (e.getSource() == botonera.grupoBotones[9]) {
+
+            areaTexto.setText(areaTexto.getText() + "9");
+
+        } else if (e.getSource() == botonera.grupoBotones[10]) {
+
+            areaTexto.setText(areaTexto.getText() + "+");
+
+        } else if (e.getSource() == botonera.grupoBotones[11]) {
+
+            areaTexto.setText(areaTexto.getText() + "-");
+
+        } else if (e.getSource() == botonera.grupoBotones[12]) {
+
+            areaTexto.setText(areaTexto.getText() + "*");
+
+        } else if (e.getSource() == botonera.grupoBotones[13]) {
+
+            areaTexto.setText(areaTexto.getText() + "/");
+
+        } else if (e.getSource() == botonera.grupoBotones[14]) {
+
+            areaTexto.setText(areaTexto.getText() + "=");
+
+        } else if (e.getSource() == botonera.grupoBotones[15]) {
+
+            if(areaTexto.getText().length() > 0){
+                
+                String subcadena = areaTexto.getText().substring(0, areaTexto.getText().length()-1);
+                areaTexto.setText(subcadena);
+            }
+
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
